2701df3ffbc291a16b973e8186b6689c70b36b96
BlessingAgbor <96949555+BlessingAgbor@users.noreply.github.com>
2022-04-08 16:57:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const express =require('express')
 const port = process.env.PORT || 4000;
 const app = express()
+const cors = require("cors")
 
 app.use(express.json())
 
@@ -12,10 +13,13 @@ app.get("/", (req, res)=>{
     res.send("Welcome to Inventory App database, where you have the Api of companies and their products")
 })
 
+
 const productRoute= require('./Routes/productRoute')
 const companyRoute= require('./Routes/companyRoute')
 // for the nodeJs to understand the routing, we need a middlware, i.e the .use
 
+//calling cors for cross communication 
+app.use(cors({origin:"*"}))
 app.use('/api/user', productRoute)
 // app.use('/api/user', companyRoute)
 app.use('/api/user', require("./Routes/userRouter"))
